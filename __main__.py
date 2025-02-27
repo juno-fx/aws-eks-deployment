c3cb1a920367e5bfe5eb768a41c897b38311dd00
aldmbmtl <alex.e.hatfield@gmail.com>
2025-02-27 12:56:16 -0500

    Fixing main file

--- __main__.py
+++ __main__.py
@@ -5,58 +5,58 @@ Juno Innovations - EKS Infrastructure for Orion
 from src import JunoAccount, JunoRegion, Cluster, set_repositories, set_profile, set_session
 
 # set the root account
-JunoAccount.set_root_account("Alex Hatfield")                 # this is the root account that will be used to manage the other accounts
+JunoAccount.set_root_account("management_account_name")                 # this is the root account that will be used to manage the other accounts
 
 # set AWS session and profiles
-set_profile("cosmos")                                      # this should be an account that can assume other accounts and have specific permissions to do so
-set_session("Cosmos")                                  # this is a trackable session name
+set_profile("management_iam_user")                                      # this should be an account that can assume other accounts and have specific permissions to do so
+set_session("management_session_name")                                  # this is a trackable session name
 
 # bootstrap
 Cluster.set_bootstrap_repository(
     repository="https://github.com/juno-fx/aws-eks-deployment.git",     # this is the repository that will be used to bootstrap the cluster
     path="bootstrap/",                                                  # this is the path within the repository that contains the bootstrap helm chart
     ref="main",                                                         # this is the branch or tag that will be used to bootstrap the cluster
-    domain="juno-innovations.com"                                                # this is the domain that will be used to create the cluster
+    domain="example.com"                                                # this is the domain that will be used to create the cluster
 )
 
 # ECR repositories
 # These are the repositories that will be used to store the images for the
 # cluster. If you are planning on bringing your own images, you can disable
 # this block.
-# set_repositories([
-#     # juno images
-#     "hubble",
-#     "kuiper",
-#     "titan",
-#     "polaris-workstation",
-#     "mars",
-#     "luna",
-#     "webb",
-#     "pluto",
-#     "terra",
-#     "mercury",
-#     "genesis"
-# ])
+set_repositories([
+    # juno images
+    "hubble",
+    "kuiper",
+    "titan",
+    "polaris-workstation",
+    "mars",
+    "luna",
+    "webb",
+    "pluto",
+    "terra",
+    "mercury",
+    "genesis"
+])
 
 
 # account and regional deployments
-with JunoAccount("Production"):                    # this is the account that will be used to deploy the clusters
+with JunoAccount("deployment_account_name"):                    # this is the account that will be used to deploy the clusters
     with JunoRegion("us-east-1", ecr_master=True):        # this is the region that the clusters will be deployed to
         pass
-        # example private cluster
-        with Cluster(private=True) as cluster:
-            # standard service node setup
-            cluster.add_node_group(
-                name="service",
-                instances=["c6a.xlarge", "t3.xlarge"],
-                capacity_type=cluster.CapacityType.SPOT,
-                minimum=1,
-                size=1,
-                maximum=5,
-                labels={
-                    "juno-innovations.com/service": "true"
-                }
-            )
+        # # example private cluster
+        # with Cluster(private=True) as cluster:
+        #     # standard service node setup
+        #     cluster.add_node_group(
+        #         name="service",
+        #         instances=["c6a.xlarge", "t3.xlarge"],
+        #         capacity_type=cluster.CapacityType.SPOT,
+        #         minimum=1,
+        #         size=1,
+        #         maximum=5,
+        #         labels={
+        #             "juno-innovations.com/service": "true"
+        #         }
+        #     )
         #
         #     # example render node setup flagged for headless workloads and is GPU enabled
         #     cluster.add_node_group(
